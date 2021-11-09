df5ea3d80187c13a2d238dab34b75b5ec9907493
lalilisa <maivantri309@gmail.com>
2021-11-09 08:50:10 +0700

    change

--- static/app-assets/js/texttospeechJS/speechtotext.js
+++ static/app-assets/js/texttospeechJS/speechtotext.js
@@ -1,3 +1,116 @@
+// arrayvocie=[
+//     {name: 'UK English Female', flag: 'gb', gender: 'f', lang: 'en-GB', voiceIDs:    ""   }
+//    ,{name: 'UK English Male', flag: 'gb', gender: 'm', lang: 'en-GB', voiceIDs:    ""   }
+//    ,{name: 'US English Female', flag: 'us', gender: 'f', lang: 'en-US', voiceIDs:    ""   }
+//    ,{name: 'US English Male', flag: 'us', gender: 'm', lang: 'en-US', voiceIDs:    ""   }
+//    ,{name: 'Arabic Male', flag: 'ar', gender: 'm', lang: 'ar-SA', voiceIDs:   ""   }
+//    ,{name: 'Arabic Female', flag: 'ar', gender: 'f', lang: 'ar-SA', voiceIDs:   ""   }
+//    ,{name: 'Armenian Male', flag: 'hy', gender: 'f', lang: 'hy-AM', voiceIDs:   ""   }
+//    ,{name: 'Australian Female', flag: 'au', gender: 'f', lang: 'en-AU', voiceIDs:   ""   }
+//    ,{name: 'Australian Male', flag: 'au', gender: 'm', lang: 'en-AU', voiceIDs:   ""   }
+//    ,{name: 'Bangla Bangladesh Female', flag: 'bd', gender: 'f', lang: 'bn-BD', voiceIDs:   ""   }
+//    ,{name: 'Bangla Bangladesh Male', flag: 'bd', gender: 'm', lang: 'bn-BD', voiceIDs:   ""   }
+//    ,{name: 'Bangla India Female', flag: 'bd', gender: 'f', lang: 'bn-IN', voiceIDs:   ""   }
+//    ,{name: 'Bangla India Male', flag: 'bd', gender: 'm', lang: 'bn-IN', voiceIDs:   ""   }
+//    ,{name: 'Brazilian Portuguese Female', flag: 'br', gender: 'f', lang: 'pt-BR', voiceIDs:   ""   }
+//    ,{name: 'Brazilian Portuguese Male', flag: 'br', gender: 'm', lang: 'pt-BR', voiceIDs:     ""   }
+//    ,{name: 'Chinese Female', flag: 'cn', gender: 'f', lang: 'zh-CN', voiceIDs:    ""   }
+//    ,{name: 'Chinese Male', flag: 'cn', gender: 'm', lang: 'zh-CN', voiceIDs:   ""   }
+//    ,{name: 'Chinese (Hong Kong) Female', flag: 'hk', gender: 'f', lang: 'zh-HK', voiceIDs:    ""   }
+//    ,{name: 'Chinese (Hong Kong) Male', flag: 'hk', gender: 'm', lang: 'zh-HK', voiceIDs:   ""   }
+//    ,{name: 'Chinese Taiwan Female', flag: 'tw', gender: 'f', lang: 'zh-TW', voiceIDs:    ""   }
+//    ,{name: 'Chinese Taiwan Male', flag: 'tw', gender: 'm', lang: 'zh-TW', voiceIDs:   ""   }
+//    ,{name: 'Czech Female', flag: 'cz', gender: 'f', lang: 'cs-CZ', voiceIDs:   ""   }
+//    ,{name: 'Czech Male', flag: 'cz', gender: 'm', lang: 'cs-CZ', voiceIDs:     ""   }
+//    ,{name: 'Danish Female', flag: 'dk', gender: 'f', lang: 'da-DK', voiceIDs:   ""   }
+//    ,{name: 'Danish Male', flag: 'da', gender: 'm', lang: 'da-DK', voiceIDs:     ""   }
+//    ,{name: 'Deutsch Female', flag: 'de', gender: 'f', lang: 'de-DE', voiceIDs:    ""   }
+//    ,{name: 'Deutsch Male', flag: 'de', gender: 'm', lang: 'de-DE', voiceIDs:   ""   }
+//    ,{name: 'Dutch Female', flag: 'nl', gender: 'f', lang: 'nl-NL', voiceIDs:   ""   }
+//    ,{name: 'Dutch Male', flag: 'nl', gender: 'm', lang: 'nl-NL', voiceIDs:   ""   }
+//    ,{name: 'Estonian Male', flag: 'ee', gender: 'm', lang: 'et-EE', voiceIDs:   ""   }
+//    ,{name: 'Filipino Female', flag: 'ph', gender: 'f', lang: 'fil-PH', voiceIDs:   ""   }
+//    ,{name: 'Finnish Female', flag: 'fi', gender: 'f', lang: 'fi-FI', voiceIDs:   ""   }
+//    ,{name: 'Finnish Male', flag: 'fi', gender: 'm', lang: 'fi-FI', voiceIDs:     ""   }
+//    ,{name: 'French Female', flag: 'fr', gender: 'f', lang: 'fr-FR', voiceIDs:    ""   }
+//    ,{name: 'French Male', flag: 'fr', gender: 'm', lang: 'fr-FR', voiceIDs:   ""   }
+//    ,{name: 'French Canadian Female', flag: 'ca', gender: 'f', lang: 'fr-CA', voiceIDs:   ""   }
+//    ,{name: 'French Canadian Male', flag: 'ca', gender: 'm', lang: 'fr-CA', voiceIDs:   ""   }
+//    ,{name: 'Greek Female', flag: 'gr', gender: 'f', lang: 'el-GR', voiceIDs:   ""   }
+//    ,{name: 'Greek Male', flag: 'gr', gender: 'm', lang: 'el-GR', voiceIDs:     ""   }
+//    ,{name: 'Hindi Female', flag: 'hi', gender: 'f', lang: 'hi-IN', voiceIDs:    ""   }
+//    ,{name: 'Hindi Male', flag: 'hi', gender: 'm', lang: 'hi-IN', voiceIDs:   ""   }
+//    ,{name: 'Hungarian Female', flag: 'hu', gender: 'f', lang: 'hu-HU', voiceIDs:   ""   }
+//    ,{name: 'Hungarian Male', flag: 'hu', gender: 'm', lang: 'hu-HU', voiceIDs:     ""   }
+//    ,{name: 'Indonesian Female', flag: 'id', gender: 'f', lang: 'id-ID', voiceIDs:   ""   }
+//    ,{name: 'Indonesian Male', flag: 'id', gender: 'm', lang: 'id-ID', voiceIDs:   ""   }
+//    ,{name: 'Italian Female', flag: 'it', gender: 'f', lang: 'it-IT', voiceIDs:    ""   }
+//    ,{name: 'Italian Male', flag: 'it', gender: 'm', lang: 'it-IT', voiceIDs:   ""   }
+//    ,{name: 'Japanese Female', flag: 'jp', gender: 'f', lang: 'ja-JP', voiceIDs:    ""   }
+//    ,{name: 'Japanese Male', flag: 'jp', gender: 'm', lang: 'ja-JP', voiceIDs:   ""   }
+//    ,{name: 'Korean Female', flag: 'kr', gender: 'f', lang: 'ko-KR', voiceIDs:    ""   }
+//    ,{name: 'Korean Male', flag: 'kr', gender: 'm', lang: 'ko-KR', voiceIDs:   ""   }
+//    ,{name: 'Latin Female', flag: 'va', gender: 'f', lang: 'la', voiceIDs:     ""   }
+//    ,{name: 'Latin Male', flag: 'va', gender: 'm', lang: 'la', voiceIDs:   ""   }
+//    ,{name: 'Nepali', flag: 'np', gender: 'f', lang: 'ne-NP', voiceIDs:   ""   }
+//    ,{name: 'Norwegian Female', flag: 'no', gender: 'f', lang: 'nb-NO', voiceIDs:   ""   }
+//    ,{name: 'Norwegian Male', flag: 'no', gender: 'm', lang: 'nb-NO', voiceIDs:   ""   }
+//    ,{name: 'Polish Female', flag: 'pl', gender: 'f', lang: 'pl-PL', voiceIDs:    ""   }
+//    ,{name: 'Polish Male', flag: 'pl', gender: 'm', lang: 'pl-PL', voiceIDs:   ""   }
+//    ,{name: 'Portuguese Female', flag: 'br', gender: 'f', lang: 'pt-BR', voiceIDs:    ""   }
+//    ,{name: 'Portuguese Male', flag: 'br', gender: 'm', lang: 'pt-BR', voiceIDs:   ""   }
+//    ,{name: 'Romanian Female', flag: 'ro', gender: 'f', lang: 'ro-RO', voiceIDs:   ""   }
+//    ,{name: 'Russian Female', flag: 'ru', gender: 'f', lang: 'ru-RU', voiceIDs:    ""   }
+//    ,{name: 'Russian Male', flag: 'ru', gender: 'm', lang: 'ru-RU', voiceIDs:     ""   }
+//    ,{name: 'Sinhala', flag: 'lk', gender: 'f', lang: 'si-LK', voiceIDs:   ""   }
+//    ,{name: 'Slovak Female', flag: 'sk', gender: 'f', lang: 'sk-SK', voiceIDs:   ""   }
+//    ,{name: 'Slovak Male', flag: 'sk', gender: 'm', lang: 'sk-SK', voiceIDs:     ""   }
+//    ,{name: 'Spanish Female', flag: 'es', gender: 'f', lang: 'es-ES', voiceIDs:    ""   }
+//    ,{name: 'Spanish Male', flag: 'es', gender: 'm', lang: 'es-ES', voiceIDs:     ""   }
+//    ,{name: 'Spanish Latin American Female', flag: 'es', gender: 'f', lang: 'es-MX', voiceIDs:    ""   }
+//    ,{name: 'Spanish Latin American Male', flag: 'es', gender: 'm', lang: 'es-MX', voiceIDs:   ""   }
+//    ,{name: 'Swedish Female', flag: 'sv', gender: 'f', lang: 'sv-SE', voiceIDs:   ""   }
+//    ,{name: 'Swedish Male', flag: 'sv', gender: 'm', lang: 'sv-SE', voiceIDs:   ""   }
+//    ,{name: 'Tamil Female', flag: 'hi', gender: 'm', lang: 'hi-IN', voiceIDs:   ""   }
+//    ,{name: 'Tamil Male', flag: 'hi', gender: 'm', lang: 'hi-IN', voiceIDs:   ""   }
+//    ,{name: 'Thai Female', flag: 'th', gender: 'f', lang: 'th-TH', voiceIDs:   ""   }
+//    ,{name: 'Thai Male', flag: 'th', gender: 'm', lang: 'th-TH', voiceIDs:   ""   }
+//    ,{name: 'Turkish Female', flag: 'tr', gender: 'f', lang: 'tr-TR', voiceIDs:   ""   }
+//    ,{name: 'Turkish Male', flag: 'tr', gender: 'm', lang: 'tr-TR', voiceIDs:   ""   }
+//    ,{name: 'Ukrainian Female', flag: 'ua', gender: 'f', lang: 'uk-UA', voiceIDs:   ""   }
+//    ,{name: 'Vietnamese Female', flag: 'vi', gender: 'f', lang: 'vi-VN', voiceIDs:   ""   }
+//    ,{name: 'Vietnamese Male', flag: 'vi', gender: 'm', lang: 'vi-VN', voiceIDs:   ""   }
+//    ,{name: 'Afrikaans Male', flag: 'af', gender: 'm', lang: 'af-ZA', voiceIDs:   ""   }
+//    ,{name: 'Albanian Male', flag: 'sq', gender: 'm', lang: 'sq-AL', voiceIDs:   ""   }
+//    ,{name: 'Bosnian Male', flag: 'bs', gender: 'm', lang: 'bs', voiceIDs:   ""   }
+//    ,{name: 'Catalan Male', flag: 'catalonia', gender: 'm', lang: 'ca-ES', voiceIDs:   ""   }
+//    ,{name: 'Croatian Male', flag: 'hr', gender: 'm', lang: 'hr-HR', voiceIDs:   ""   }
+//    ,{name: 'Esperanto Male', flag: 'eo', gender: 'm', lang: 'eo', voiceIDs:   ""   }
+//    ,{name: 'Icelandic Male', flag: 'is', gender: 'm', lang: 'is-IS', voiceIDs:     ""   }
+//    ,{name: 'Icelandic Female', flag: 'is', gender: 'm', lang: 'is-IS', voiceIDs:   ""   }
+//    ,{name: 'Latvian Male', flag: 'lv', gender: 'm', lang: 'lv-LV', voiceIDs:   ""   }
+//    ,{name: 'Macedonian Male', flag: 'mk', gender: 'm', lang: 'mk-MK', voiceIDs:   ""   }
+//    ,{name: 'Moldavian Female', flag: 'md', gender: 'f', lang: 'md', voiceIDs:   ""   }
+//    ,{name: 'Moldavian Male', flag: 'md', gender: 'm', lang: 'md', voiceIDs:     ""   }
+//    ,{name: 'Montenegrin Male', flag: 'me', gender: 'm', lang: 'me', voiceIDs:   ""   }
+//    ,{name: 'Serbian Male', flag: 'sr', gender: 'm', lang: 'sr-RS', voiceIDs:   ""   }
+//    ,{name: 'Serbo-Croatian Male', flag: 'hr', gender: 'm', lang: 'hr-HR', voiceIDs:   ""   }
+//    ,{name: 'Swahili Male', flag: 'sw', gender: 'm', lang: 'sw-KE', voiceIDs:   ""   }
+//    ,{name: 'Welsh Male', flag: 'cy', gender: 'm', lang: 'cy', voiceIDs:   ""   }
+//    ,{name: 'Fallback UK Female', flag: 'gb', gender: 'f', lang: 'en-GB', voiceIDs:   ""   },
+// ]
+// var objvocie=null;
+// var reslutvoice={speak: 'US English Female',lang:'en-US'};
+// var langoptionvocie=$('#select1').val();
+// obj=arrayvocie.find(o=>o.lang===langoptionvocie);
+// reslutvocie.speak=obj.name;
+// reslutvocie.lang=obj.lang;
+// function changeoption(){
+//     langoption=$('#select1').val();
+//     objvocie=arrayvocie.find(o=>o.lang===langoptionvocie);
+//     reslutvoice.speak=objvocie.name;
+//     reslutvoice.lang=objvocie.lang;
+// }
 
 window.SpeechRecognition =
 window.SpeechRecognition ||
@@ -23,13 +136,14 @@ var t = document.getElementById("recognize-btn");
        recognition.stop();
        t.value="NO";
        $('#textscan').prop('disabled', false);
-       
+       $('#select1').prop('disabled', false);
         document.getElementById('recognize-btn').setAttribute("style","color:black");
     }
     else if(t.value==="NO"){
         t.value="YES";
         recognition.start();
          $('#textscan').prop('disabled', true);
+       $('#select1').prop('disabled', true);
         document.getElementById('textscan').setAttribute("style","background-color:#F8F8F8;")
         document.getElementById('recognize-btn').setAttribute("style","color:red");
     }
