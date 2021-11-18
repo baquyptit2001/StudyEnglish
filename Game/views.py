e800cefb3c0b007b73aaa6d2623f925a06d60448
baquyptit2001 <quynb201@gmail.com>
2021-11-18 21:46:20 +0700

    add question

--- Game/views.py
+++ Game/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-from Game.models import Question
+from Game.models import Question, Answer
 
 
 def home(request):
-    return render(request, 'index.html')
+    question = Question.objects.order_by('?')[0]
+    answer = question.answer_set.order_by('?')
+    return render(request, 'index.html', {"question": question, 'answer': answer})
 
 
 def add_question(request):
@@ -15,13 +17,16 @@ def add_question(request):
         question = Question.objects.create(
             name=request.POST['question'],
         )
+        question.save()
         status = 'true'
         if question is not None:
-            for ans in request.POST['answer']:
-                question.answer.create(
-                    answer=ans,
+            for i in range(1, 5):
+                Answer.objects.create(
+                    question=question,
+                    answer=request.POST['answer' + str(i)],
                     status=status
                 )
+                question.save()
                 status = 'false'
         text = request.POST['question']
     return render(request, 'add-question.html', {"text": text})
