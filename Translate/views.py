d13752db61c075017fbb1585a44935d86fad90a5
lalilisa <maivantri309@gmail.com>
2021-11-18 09:36:52 +0700

    tri

--- Translate/views.py
+++ Translate/views.py
@@ -1,16 +1,26 @@
+from typing import Text
 from django.shortcuts import render
 from googletrans import Translator
+from Translate.models import Languages
+
 # Create your views here.
 def translate(request):
+    languages=Languages.objects.all()
     if request.method == "POST":
-            srcs = str(request.POST.get("src",None))
-            srcs=(srcs.split('-'))[0]
             txt = request.POST.get("text", None)
-            
+            srcs = str(request.POST.get("src",None))
+            srcs1=(srcs.split('-'))[0]
+            if srcs=='zh-CN':
+                   srcs1='zh-CN'
             des = request.POST.get("des",None)
-            des=(des.split('-'))[0]
-            translator = Translator()
-            t=translator.translate(txt,src=srcs, dest=des)
+            des1=(des.split('-'))[0]
             context={"srcs":srcs,"des":des}
-            return render(request, 'translate.html', {"input":txt,"output":t.text,"context":context})
-    return render(request,'translate.html')
+            if des=='zh-CN':
+                    des1='zh-CN'
+            if txt == "":
+                    return render(request, 'translate.html', {"input":"","output":"","context":context,"languages":languages})
+            translator = Translator()
+            t=translator.translate(txt,src=srcs1, dest=des1)
+            
+            return render(request, 'translate.html', {"input":txt,"output":t.text,"context":context,"languages":languages})
+    return render(request,'translate.html', {"languages":languages})
--- static/app-assets/js/texttospeechJS/texttospeech.js
+++ static/app-assets/js/texttospeechJS/texttospeech.js
@@ -149,44 +149,7 @@ function textSpeakTranslated(){
     if (lang.indexOf('_') !== -1)
         lang = lang.split('_')[0];
     var voice=resluted.speak;
-    // var voice='UK English Female';
+
     var text=document.getElementById("aftertranslate").value;
     responsiveVoice.speak(text,voice);
 }
-
-// window.SpeechRecognition =
-// window.SpeechRecognition ||
-//  window.webkitSpeechRecognition;
-// const recognition = new SpeechRecognition();
-// recognition.interimResults = true;
-// recognition.continuous = true;
-
-
-// var textarea=$("#textscan");
-// recognition.addEventListener("result", (e) => {
-// const text = Array.from(e.results)
-//   .map((result) => result[0])
-//   .map((result) => result.transcript)
-//    .join("");
-//     textarea.val(text)
-// });
-
-// var t = document.getElementById("recognize-btn");
-//  function micro() {
-//     recognition.lang=reslut.lang;
-//     if(t.value==="YES"){
-//        recognition.stop();
-//        t.value="NO";
-//        $('#textscan').prop('disabled', false);
-       
-//         document.getElementById('recognize-btn').setAttribute("style","color:black");
-//     }
-//     else if(t.value==="NO"){
-//         t.value="YES";
-//         recognition.start();
-//          $('#textscan').prop('disabled', true);
-//         document.getElementById('textscan').setAttribute("style","background-color:#F8F8F8;")
-//         document.getElementById('recognize-btn').setAttribute("style","color:red");
-//     }
-    
-// }
